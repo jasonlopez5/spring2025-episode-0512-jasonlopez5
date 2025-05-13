e382db780a0231b527039561eb3405e3c6503e0c
Jason Lopez <jasonlopez16@icloud.com>
2025-05-12 21:56:22 -0700

    added subtraction

---
+++ subtraction.py
@@ -0,0 +1,23 @@
+#subtraction module 
+def subtract(subtracts: list[float]) -> float:
+    if not subtracts:
+        return 0
+
+    difference = subtracts[0]
+    for number in subtracts[1:]:
+        difference = difference - number
+
+    return difference
+
+def describe_subtract() -> str:
+    return 'Returns the sum of all of the numbers passed to it.'
+
+
+if __name__ == '__main__':
+    import sys
+
+    print(describe_subtract())
+
+    if len(sys.argv) > 1:
+        subtracts = [float(arg) for arg in sys.argv[1:]]
+        print(subtract(subtracts))
