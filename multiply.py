f23e91f475c46e849efb18cafff7128a4a2fcdb4
Jason Lopez <jasonlopez16@icloud.com>
2025-05-12 22:26:46 -0700

    added multiply

---
+++ multiply.py
@@ -0,0 +1,18 @@
+def multiply(factors: list[float]) -> float:
+    product = 1
+    for factor in factors:
+        product = product * factor
+
+    return product
+
+def describe_multiply() -> str:
+    return 'Returns the product of all of the numbers passed to it.'
+
+if __name__ == '__main__':
+    import sys
+
+    print(describe_multiply())
+
+    if len(sys.argv) > 1:
+        factors = [float(arg) for arg in sys.argv[1:]]
+        print(multiply(factors))
